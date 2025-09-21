2980b62c9cd206675f8a1b0081d1b7f2dfddc5cb
santhosh <santhoshchekuri2002@gmail.com>
2025-09-21 16:49:41 +0530

    docker

--- src/main/java/com/capstone/admin/api/AdminController.java
+++ src/main/java/com/capstone/admin/api/AdminController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
-//@CrossOrigin
+@CrossOrigin
 public class AdminController {
     @Autowired
     private AdminService adminService;
